9cbb04b0b9a604fa517b7a5f7f4caaefca3019d1
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-07-24 08:51:23 +0100

    Add subtests

--- iteration/iteration_test.go
+++ iteration/iteration_test.go
@@ -3,12 +3,33 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", 6)
-	expected := "aaaaaa"
 
-	if repeated != expected {
-		t.Errorf("Expected %q but got %q", expected, repeated)
-	}
+	t.Run("test it repeates the characted 6 times", func(t *testing.T) {
+		repeated := Repeat("a", 6)
+		expected := "aaaaaa"
+
+		if repeated != expected {
+			t.Errorf("Expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("test it uses default count if counter is 0", func(t *testing.T) {
+		repeated := Repeat("a", 0)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("Expected %q but got %q", expected, repeated)
+		}
+	})
+
+	t.Run("test it uses default count if counter is -1", func(t *testing.T) {
+		repeated := Repeat("a", -1)
+		expected := "aaaaa"
+
+		if repeated != expected {
+			t.Errorf("Expected %q but got %q", expected, repeated)
+		}
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
