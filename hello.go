dd204f6ff827edfce773a1f9b179aa9c17112552
Dimitris Stoikidis <dimitrios.stoikidis@sky.uk>
2020-07-24 08:11:40 +0100

    first commit

---
+++ hello.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+const french = "French"
+const spanish = "Spanish"
+const englishHelloPrefix = "Hello, "
+const frenchHelloPrefix = "Allo, "
+const spanishHelloPrefix = "Hola, "
+
+func Hello(name string, language string) string {
+	if name == "" {
+		name = "World"
+	}
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case french:
+		prefix = frenchHelloPrefix
+	case spanish:
+		prefix = spanishHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+	return
+}
+
+func main() {
+	fmt.Println(Hello("Chris", "English"))
+}
