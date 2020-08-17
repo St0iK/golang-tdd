fed2e9ad79cf19228b45de21a27346daae284444
Dimitris Stoikidis <jstoikidis@gmail.com>
2020-08-17 08:08:24 +0100

    Json encoding

--- server/in_memory_player_store.go
+++ server/in_memory_player_store.go
@@ -1,32 +1,30 @@
 package main
 
-import "sync"
-
 // NewInMemoryPlayerStore initialises an empty player store.
 func NewInMemoryPlayerStore() *InMemoryPlayerStore {
-	return &InMemoryPlayerStore{
-		map[string]int{},
-		sync.RWMutex{},
-	}
+	return &InMemoryPlayerStore{map[string]int{}}
 }
 
 // InMemoryPlayerStore collects data about players in memory.
 type InMemoryPlayerStore struct {
 	store map[string]int
-	// A mutex is used to synchronize read/write access to the map
-	lock sync.RWMutex
+}
+
+// GetLeague returns a collection of Players.
+func (i *InMemoryPlayerStore) GetLeague() []Player {
+	var league []Player
+	for name, wins := range i.store {
+		league = append(league, Player{name, wins})
+	}
+	return league
 }
 
 // RecordWin will record a player's win.
 func (i *InMemoryPlayerStore) RecordWin(name string) {
-	i.lock.Lock()
-	defer i.lock.Unlock()
 	i.store[name]++
 }
 
 // GetPlayerScore retrieves scores for a given player.
 func (i *InMemoryPlayerStore) GetPlayerScore(name string) int {
-	i.lock.RLock()
-	defer i.lock.RUnlock()
 	return i.store[name]
 }
--- server/server_test.go
+++ server/server_test.go
@@ -1,15 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io"
 	"net/http"
 	"net/http/httptest"
+	"reflect"
 	"testing"
 )
 
 type StubPlayerStore struct {
 	scores   map[string]int
 	winCalls []string
+	league   []Player
 }
 
 func (s *StubPlayerStore) GetPlayerScore(name string) int {
@@ -21,6 +25,10 @@ func (s *StubPlayerStore) RecordWin(name string) {
 	s.winCalls = append(s.winCalls, name)
 }
 
+func (s *StubPlayerStore) GetLeague() []Player {
+	return s.league
+}
+
 func TestGETPlayers(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{
@@ -28,58 +36,37 @@ func TestGETPlayers(t *testing.T) {
 			"Floyd":  10,
 		},
 		nil,
+		nil,
 	}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
-	tests := []struct {
-		name               string
-		player             string
-		expectedHTTPStatus int
-		expectedScore      string
-	}{
-		{
-			name:               "Returns Pepper's score",
-			player:             "Pepper",
-			expectedHTTPStatus: http.StatusOK,
-			expectedScore:      "20",
-		},
-		{
-			name:               "Returns Floyd's score",
-			player:             "Floyd",
-			expectedHTTPStatus: http.StatusOK,
-			expectedScore:      "10",
-		},
-		{
-			name:               "Returns 404 on missing players",
-			player:             "Apollo",
-			expectedHTTPStatus: http.StatusNotFound,
-			expectedScore:      "0",
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			request := newGetScoreRequest(tt.player)
-			response := httptest.NewRecorder()
+	t.Run("returns Pepper's score", func(t *testing.T) {
+		request := newGetScoreRequest("Pepper")
+		response := httptest.NewRecorder()
 
-			server.ServeHTTP(response, request)
+		server.ServeHTTP(response, request)
 
-			assertStatus(t, response.Code, tt.expectedHTTPStatus)
-			assertResponseBody(t, response.Body.String(), tt.expectedScore)
-		})
-	}
-}
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "20")
+	})
 
-func TestLeague(t *testing.T) {
-	store := StubPlayerStore{}
-	server := &PlayerServer{&store}
+	t.Run("returns Floyd's score", func(t *testing.T) {
+		request := newGetScoreRequest("Floyd")
+		response := httptest.NewRecorder()
 
-	t.Run("it returns 200 on /league", func(t *testing.T) {
-			request, _ := http.NewRequest(http.MethodGet, "/league", nil)
-			response := httptest.NewRecorder()
+		server.ServeHTTP(response, request)
 
-			server.ServeHTTP(response, request)
+		assertStatus(t, response.Code, http.StatusOK)
+		assertResponseBody(t, response.Body.String(), "10")
+	})
 
-			assertStatus(t, response.Code, http.StatusOK)
+	t.Run("returns 404 on missing players", func(t *testing.T) {
+		request := newGetScoreRequest("Apollo")
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		assertStatus(t, response.Code, http.StatusNotFound)
 	})
 }
 
@@ -87,8 +74,9 @@ func TestStoreWins(t *testing.T) {
 	store := StubPlayerStore{
 		map[string]int{},
 		nil,
+		nil,
 	}
-	server := &PlayerServer{&store}
+	server := NewPlayerServer(&store)
 
 	t.Run("it records wins on POST", func(t *testing.T) {
 		player := "Pepper"
@@ -110,13 +98,67 @@ func TestStoreWins(t *testing.T) {
 	})
 }
 
+func TestLeague(t *testing.T) {
 
+	t.Run("it returns the league table as JSON", func(t *testing.T) {
+		wantedLeague := []Player{
+			{"Cleo", 32},
+			{"Chris", 20},
+			{"Tiest", 14},
+		}
+
+		store := StubPlayerStore{nil, nil, wantedLeague}
+		server := NewPlayerServer(&store)
+
+		request := newLeagueRequest()
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+
+		got := getLeagueFromResponse(t, response.Body)
+
+		assertStatus(t, response.Code, http.StatusOK)
+		assertLeague(t, got, wantedLeague)
+		assertContentType(t, response, jsonContentType)
+
+	})
+}
+
+func assertContentType(t *testing.T, response *httptest.ResponseRecorder, want string) {
+	t.Helper()
+	if response.Result().Header.Get("content-type") != want {
+		t.Errorf("response did not have content-type of %s, got %v", want, response.Result().Header)
+	}
+}
+
+func getLeagueFromResponse(t *testing.T, body io.Reader) (league []Player) {
+	t.Helper()
+	err := json.NewDecoder(body).Decode(&league)
+
+	if err != nil {
+		t.Fatalf("Unable to parse response from server %q into slice of Player, '%v'", body, err)
+	}
+	fmt.Printf("Value %v", league)
+	return 
+}
+
+func assertLeague(t *testing.T, got, want []Player) {
+	t.Helper()
+	if !reflect.DeepEqual(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
 
 func assertStatus(t *testing.T, got, want int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("did not get correct status, got %d, want %d", got, want)
 	}
+}
+
+func newLeagueRequest() *http.Request {
+	req, _ := http.NewRequest(http.MethodGet, "/league", nil)
+	return req
 }
 
 func newGetScoreRequest(name string) *http.Request {
